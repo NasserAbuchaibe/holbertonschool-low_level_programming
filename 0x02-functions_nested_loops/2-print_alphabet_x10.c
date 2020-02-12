337fed32c44a7c52f7bc84073311fc019ee0d862
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-02-12 04:14:42 +0000

    alphab10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
  * print_alphabet_x10 - print alphabet x10
  * @x: counter to do 10 cycles
@@ -11,10 +10,10 @@ void print_alphabet_x10(void)
 
 	for (x = 1 ; x <= 10 ; x++)
 	{
-		for (alph = 'a' ; alph <= 'z' ; alph++)
-		{
-		 _putchar(alph);
-		}
+	for (alph = 'a' ; alph <= 'z' ; alph++)
+	{
+	 _putchar(alph);
+	}
 	_putchar('\n');
 	}
 }
