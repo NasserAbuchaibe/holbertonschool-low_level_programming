feb69d40848f03bf288d9b12cbe889d73e2840ba
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-02-12 02:49:03 +0000

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,15 +5,14 @@
  *
  * Return: Always 0.
  */
-
 int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
-	return (1);
+		return (1);
 	}
 	else
 	{
-	return (0);
+		return (0);
 	}
 }
