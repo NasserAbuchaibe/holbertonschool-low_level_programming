93df9667a936ca3acaabd444aaee094ddf3f3481
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-02-12 03:08:44 +0000

    2sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,19 +5,18 @@
  *
  * Return: Always 0.
  */
-
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-	return (1);
+		return (1);
 	}
 	else if (n == 0)
 	{
-	return (0);
+		return (0);
 	}
 	else
 	{
-	return (-1);
+		return (-1);
 	}
 }
