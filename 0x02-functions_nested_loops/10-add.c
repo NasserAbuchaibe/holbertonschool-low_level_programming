89c8a3a8823d5dc4c66281e0253c016ca206dcc3
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-02-12 04:58:32 +0000

    add1

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -12,18 +12,4 @@ int add(int a, int b)
 
 	suma = a + b;
 	return (suma);
-}#include "holberton.h"
-
-/**
-* add - Function that prints two integers
-* @a: integer
-* @b: integer
-* Return: suma
-*/
-int add(int a, int b)
-{
-	int suma = 0;
-
-	suma = a + b;
-	return (suma);
 }
