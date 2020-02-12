48eed936ca81d8bd569a67a18bec92e05a25b9ba
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-02-12 04:40:19 +0000

    24h

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,11 +1,38 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * jack_bauer - prints every minute of the day starting from 00:00 to 23:59.
+ * @a - @b - @c, @d: integers.
  *
- * Return: Always 0.
+ * Return: 0.
  */
-void times_table(void)
+void jack_bauer(void)
 {
-       
+	int a, b, c, d;
+
+	for (a = '0' ; a <= '2' ; a++)
+	{
+		for (b = '0' ; b <= '9' ; b++)
+		{
+			for (c = '0' ; c <= '5' ; c++)
+			{
+				for (d = '0' ; d <= '9' ; d++)
+				{
+				if (!(a == '2' && b == '4' && c == '0' && d == '0'))
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(':');
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}
+				else
+				{
+					return;
+				}
+				}
+			}
+		}
+	}
 }
