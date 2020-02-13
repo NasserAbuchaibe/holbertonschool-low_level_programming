a4542360cd28ebca15a6515963a04463b9c1eafc
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-02-13 04:21:15 +0000

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - function that prints if integer
+* Return: 0
+*/
+
+int main(void)
+{
+	int x, su;
+
+	for (x = 0; x <= 1024; i++)
+	{
+		if ((i % 5 == 0) || (i % 3 == 0))
+		{
+			su = su + x;
+		}
+	}
+		printf("%d\n", su);
+		return (0);
+}
