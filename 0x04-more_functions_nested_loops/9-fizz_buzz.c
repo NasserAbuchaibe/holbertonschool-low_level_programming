030ee877824967816d297ca1642947660a604c50
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-02-14 04:29:53 +0000

    fizz2

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,24 +13,21 @@ int main(void)
 		if (x % 3 == 0 && x % 5 == 0)
 		{
 			printf("FizzBuzz");
-			printf(" ");
 		}
 		else if (x % 5 == 0)
 		{
 			printf("Buzz");
-			printf(" ");
 		}
 		else if (x % 3 == 0)
 		{
 			printf("Fizz");
-			printf(" ");
 		}
 		else
 			printf("%d", x);
-			if (x != 100)
-			{
-				putchar(' ');
-			}
+		if (x != 100)
+		{
+			putchar(' ');
+		}
 	}
 	printf("\n");
 	return (0);
