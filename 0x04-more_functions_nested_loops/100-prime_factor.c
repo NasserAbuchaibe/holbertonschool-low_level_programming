5ba4c89c7b434a8a17b5067b1010bd879e3ea446
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-02-15 04:19:01 +0000

    Pfactor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 100-prime_factor.c(Success)
+ */
+int main(void)
+{
+	long int n = 612852475143;
+	long int d;
+	long int aux;
+
+	for (d = 2; d <= n; d++)
+	{
+		if (n % d == 0)
+		{
+			aux = d;
+			while (n % d == 0)
+			{
+				n /= d;
+			}
+		}
+	}
+	printf("%ld\n", aux);
+	
+	return (0);
+}
+
