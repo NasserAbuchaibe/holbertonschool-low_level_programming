4439c35f03e615693df3c8d27b201afb99a5ca52
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-02-17 20:54:10 +0000

    printRev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_rev - print reverse a string
+ * Return: void
+ * @s: string
+ */
+
+void print_rev(char *s)
+{
+	char aux;
+	int x, y, cont;
+
+	aux = s[0];
+	for (x = 0; aux != '\0'; x++)
+	{
+		aux = s[x];
+		cont = x;
+	}
+	for (y = cont; y >= 0; y--)
+	{
+		_putchar(s[y]);
+	}
+	_putchar('\n');
+}
