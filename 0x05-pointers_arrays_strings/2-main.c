1461b440c4c3e1d21a367cfd6883b07476f4e94e
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-02-17 20:28:43 +0000

    update3

--- 0x05-pointers_arrays_strings/2-main.c
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -15,21 +15,4 @@ int main(void)
     len = _strlen(str);
     printf("%d\n", len);
     return (0);
-}#include "holberton.h"
-#include <stdio.h>
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char *str;
-    int len;
-
-    str = "Holberton!";
-    len = _strlen(str);
-    printf("%d\n", len);
-    return (0);
 }
--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -12,18 +12,4 @@ int main(void)
     str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
     _puts(str);
     return (0);
-}#include "holberton.h"
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char *str;
-
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    _puts(str);
-    return (0);
 }
