aec05b2ab55d10885afec5469873446cf50152e5
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-02-17 22:40:42 +0000

    reverString

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * rev_string - reverse a string
+ * Return: void
+ * @s: string
+ */
+
+void rev_string(char *s)
+{
+	char aux;
+	int x, cont;
+
+	aux = s[0];
+	for (x = 0; aux != '\0'; x++)
+	{
+		aux = s[x];
+		cont = x;
+	}
+	for (x = 0; x <= cont; x++)
+	{
+		cont--;
+		aux = s[x];
+		s[x] = s[cont];
+		s[cont] = aux;
+	}
+}
