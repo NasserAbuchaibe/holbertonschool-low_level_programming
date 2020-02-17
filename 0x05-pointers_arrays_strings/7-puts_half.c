a351f04226b883de6e10f3409c10d8e27414cafd
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-02-17 23:52:29 +0000

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * puts_half - Write a function that prints a string
+ * @str: string
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	char aux;
+	int  cont = 0, x, y;
+
+	aux = str[0];
+	while (aux != '\0')
+	{
+		cont++;
+		aux = str[cont];
+	}
+		if ( cont % 2 == 0)
+		{
+			x = (cont -1) / 2;
+			for (y = x; y <cont; y++)
+			{
+			_putchar(str[y + 1]);
+			}
+		}
+		else
+		{
+			x = cont / 2;
+			for (y = x; y <cont; y++)
+			{
+				_putchar(str[y]);
+			}
+		}
+}
