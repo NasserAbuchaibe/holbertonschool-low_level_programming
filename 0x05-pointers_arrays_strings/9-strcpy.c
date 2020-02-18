0905a55f48a67e005442b82d6001d39aec6c3c1f
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-02-18 20:39:40 +0000

    copyArray

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strcpy - copy an array to another array
+ * @dest: destination array
+ * @src: origin array
+ * Return: void
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int x;
+
+	for (x = 0; src[x]!= '\0'; x++)
+	{
+		dest[x] = src[x];
+	}
+	return (dest);
+}
