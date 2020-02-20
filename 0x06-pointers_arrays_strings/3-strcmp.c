a8ee5b7f45e180adf3bde6a18890131f5c937043
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-02-20 21:34:33 +0000

    compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strcmp - compare two strings
+ * @dest: string 1
+ * @src: string 2
+ * @n: integer
+ * Return: dest
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int x, y;
+
+	for (x = 0; s1[x] != '\0'; x++)
+	{
+	}
+	for (y = 0; s2[y] != '\0'; y++)
+	{
+	}
+	return(x - y);
+}
+
