603b01330dfb73668041b10c0c4870000ecae7d5
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-02-21 05:06:27 +0000

    rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * rot13 - function that encodes a string into rot13.
+ * @c: string to encodes
+ * Return: char
+ */
+char *rot13(char *c)
+{
+	char rot[] = {'A', 'N', 'a', 'n', 'B', 'O', 'b', 'o',
+		'C', 'P', 'c', 'p', 'D', 'Q', 'd', 'q', 'E', 'R',
+		'e', 'r', 'F', 'S', 'f', 's', 'G', 'T', 'g', 't',
+		'H', 'U', 'h', 'u', 'I', 'V', 'i', 'v', 'J', 'W',
+		'j', 'w', 'K', 'X', 'k', 'x', 'L', 'Y', 'l', 'y',
+		'M', 'Z', 'm', 'z'};
+	int x, y, code;
+
+	code = 13;
+
+	for (x = 0; c[x] != '\0'; x++)
+	{
+		for (y = 0; y <= 51; y++)
+		{
+			if (c[x] == rot[y])
+			{
+				c[x] = c[x] + code;
+				y = 51;
+			}
+		code = code * -1;
+		}
+	}
+	return (c);
+}
