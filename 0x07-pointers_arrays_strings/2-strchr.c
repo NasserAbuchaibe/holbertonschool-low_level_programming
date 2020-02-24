ad5fb6fab0eebcb5395bece095e72b33cb6f4322
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-02-24 17:18:48 +0000

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _strchr - Write a function that locates a character in a string
+ * @s: string
+ * @c: to locate
+ * Return: null o pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	int x;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		if (s[x] == c)
+		{
+			return (s + x);
+			break;
+		}       
+	}
+	return ('\0');
+}
