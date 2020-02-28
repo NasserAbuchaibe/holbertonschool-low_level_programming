e5bf507b8600320fab610430f73f623fdb994fba
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-02-28 22:12:44 +0000

    mult

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multi2 number
+ * @argc: The number of command line arguments
+ * @argv: An array containing the program command line arguments
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int x, y;
+
+	if (argc <= 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	x = x * y;
+	printf("%d\n", x);
+	return (0);
+}
