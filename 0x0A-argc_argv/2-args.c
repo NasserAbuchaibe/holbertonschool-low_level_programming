4baf8392d62efdab321beaaa2cbbca6a11507bc0
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-02-28 23:47:14 +0000

    fix1

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,13 +5,13 @@
  * @argv: An array containing the program command line arguments
  * Return: int
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int x;
 
 	for (x = 0; x < argc; x++)
 	{
-		printf("%s\n",argv[x]);
+		printf("%s\n", argv[x]);
 	}
 	return (0);
 }
