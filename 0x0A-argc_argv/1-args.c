6441a23fdf41d75617c8bb42a06817f32cf17447
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-02-28 21:07:15 +0000

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -  prints the number of arguments passed into it
+ * @argc: The number of command line arguments
+ * @argv: An array containing the program command line arguments
+ * Return: int
+ */
+int main (int argc, char *argv[])
+{
+	printf("%d\n",argc -1);
+	(void) argv;
+	return (0);
+}
