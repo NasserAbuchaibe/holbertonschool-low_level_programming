e2fa88b62abdf461661a4be9b1ae29fd4d1efb78
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-03 18:11:47 +0000

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,18 +15,11 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 	{
-		return ('\0');
+		return (0);
 	}
 	else
 	{
 		n = malloc(size * sizeof(c));
-		if (n == 0)
-		{
-			return ('\0');
-		}
-		else
-		{
-			return (n);
-		}
+		return (n);
 	}
 }
