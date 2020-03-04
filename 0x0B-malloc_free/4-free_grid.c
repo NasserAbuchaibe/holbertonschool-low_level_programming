006bfdb66d3c045d8987348b96b780da922077c4
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-04 02:18:13 +0000

    free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2 dimensional grid
+ * @grid: grid
+ * @height: string2
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int x;
+
+	if ((grid != 0) || (height != 0))
+	{
+		for (x = 0; x < height; x++)
+		{
+			free(grid[x]);
+		}
+		free(grid);
+	}
+}
+
