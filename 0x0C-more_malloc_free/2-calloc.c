2eadef429a4def184b9a65fb8e692b1d17f318ff
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-05 21:30:02 +0000

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,12 +17,17 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(nmemb * size);
-	if (p == NULL)
-		return (NULL);
 	p1 = p;
-	for (x = 0; x < nmemb; x++)
+	if (p == NULL)
 	{
-		p1[x] = 0;
+		return (NULL);
+	}
+	else
+	{
+		for (x = 0; x < nmemb; x++)
+		{
+			p1[x] = '\0';
+		}
 	}
 	return (p1);
 }
