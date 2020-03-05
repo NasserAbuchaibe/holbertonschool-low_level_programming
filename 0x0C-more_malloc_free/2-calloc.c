bd1a69a257e03f68f99ec3f4f7583c0f723d02d8
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-05 21:10:54 +0000

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * _calloc - concatenates two strings.
+ * @nmemb: number element
+ * @size: size type
+ * Return: pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int x;
+	void *p;
+	char *p1;
+
+	if (nmemb == 0 && size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	p1 = p;
+	for (x = 0; x < nmemb; x++)
+	{
+		p1[x] = '\0';
+	}
+	return (p1);
+}
