15d2e8253e46dad667642a18647c52b2bb2197fa
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-09 18:54:44 +0000

    struc

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
