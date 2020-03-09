ae89696da60d22a18bb10134ed11464eb4559af5
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-09 21:25:59 +0000

    print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,9 +10,9 @@ void print_dog(struct dog *d)
 	if (d != 0)
 	{
 		if ((d->name) == 0)
-			(*d).name = "nil";
+			(*d).name = "(nil)";
 		if ((d->owner) == 0)
-			(*d).owner = "nil";
+			(*d).owner = "(nil)";
 		printf("Name: %s\n", (*d).name);
 		printf("Age: %f\n", (*d).age);
 		printf("Owner: %s\n", (*d).owner);
