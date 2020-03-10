2e52d3e193b8e67b848231b64f48360972b1000e
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-10 00:42:19 +0000

    new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - function that creates a new dog.
+ * @name: string
+ * @age: float
+ * @owner: string
+ * Return: pointer
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *pt;
+
+	pt = malloc(sizeof(dog_t));
+	if (pt == 0)
+		return (NULL);
+	if (name == 0)
+	{
+		free(owner);
+		free(pt);
+		return (NULL);
+	}
+	if (owner == 0)
+	{
+		free(name);
+		free(pt);
+		return (NULL);
+	}
+	pt->name = name;
+	pt->age = age;
+	pt->owner = owner;
+	return (pt);
+}
