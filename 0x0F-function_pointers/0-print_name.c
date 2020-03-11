d6ddc1f2a239591e606c1c14f2829703b2022a58
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-11 16:18:09 +0000

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+/**
+ * print_name -  function that prints a name.
+ * @name: string to print
+ * @f: function pointer
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if ((name != 0) && (f != 0))
+	{
+		f(name);
+	}
+}
+
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,7 @@
-fndef HEADER_FILE
+#ifndef HEADER_FILE
 #define HEADER_FILE
 
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int _putchar(char c);
 
