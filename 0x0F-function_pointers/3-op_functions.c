08a22eabedb2b6495c80808b7257e22c970eac5e
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-12 01:32:17 +0000

    op

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+
+/**
+ * op_add - add 2 number
+ * @a: number 1
+ * @b: number 2
+ * return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
