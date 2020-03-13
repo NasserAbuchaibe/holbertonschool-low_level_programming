b92b1161b3ecf04ce113126a88ea5f2f7862014e
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-13 20:18:13 +0000

    print s

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings, followed by a new line.
+ * @separator: const char
+ * @n: const unsigned int
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list m;
+	unsigned int x;
+	char *p;
+
+	va_start(m, n);
+		for (x = 0; x < n; x++)
+		{
+			p = va_arg(m, char *);
+			if (p == NULL)
+			{
+				printf("(nil)");
+			}
+			else
+			{
+				printf("%s", p);
+			}
+			if (x < n - 1)
+				printf("%c ", *separator);
+		}
+	printf("\n");
+	va_end(m);
+}
