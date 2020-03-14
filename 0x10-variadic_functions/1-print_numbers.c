9a3a35bd5c6b7105e3b758322b5c22a2dfc719df
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-14 17:50:43 +0000

    fix2

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			{
 				printf("%d", va_arg(m, const unsigned int));
 				if (x < n - 1)
-					printf("%c ", *separator);
+					printf("%s ", separator);
 			}
 			if (separator == NULL)
 				printf("%d", va_arg(m, const unsigned int));
