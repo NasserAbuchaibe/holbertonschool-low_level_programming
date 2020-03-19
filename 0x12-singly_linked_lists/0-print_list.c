0c5b8641bb8bac49ce5c10c0376ec9dc78207f89
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-19 22:20:05 +0000

    print linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * print_list - print linked list
+ * @h: inked list head
+ * Return: number of node
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count;
+
+	if (h == NULL)
+		return (0);
+	for (count = 0; h != NULL; count++)
+	{
+		if ((*h).str == NULL)
+		{
+			printf("[%d] %s\n", (*h).len, "(nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", (*h).len, (*h).str);
+		}
+		h = (*h).next;
+		count += 1;
+	}
+	return (count);
+}
+
