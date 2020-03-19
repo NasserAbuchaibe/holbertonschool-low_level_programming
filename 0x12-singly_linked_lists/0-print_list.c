97d1037df4c3f57630fdae50e9e93e24ee0c87ec
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-19 22:47:37 +0000

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,14 +15,13 @@ size_t print_list(const list_t *h)
 	{
 		if ((*h).str == NULL)
 		{
-			printf("[%d] %s\n", (*h).len, "(nil)");
+			printf("[%d] %s\n", 0, "(nil)");
 		}
 		else
 		{
 			printf("[%d] %s\n", (*h).len, (*h).str);
 		}
 		h = (*h).next;
-		count += 1;
 	}
 	return (count);
 }
