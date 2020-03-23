c5a141ee6520e6a49c131bc04571378e811079d9
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-23 20:14:17 +0000

    add end

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - print linked list
+ * @head: inked list head
+ * @n:number
+ * Return: pointer to new node or NULL
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new, *aux;
+
+	aux = *head;
+	new = malloc(sizeof(listint_t));
+	(*new).n = n;
+	(*new).next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	while ((*aux).next != NULL)
+	{
+		aux = (*aux).next;
+	}
+	(*aux).next = new;
+	return (new);
+}
