282b9771470aa7cace4b927ba9567b1b671641b8
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-23 21:45:26 +0000

    free2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - free list
+ * @head: inked list head
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	if (*head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		free(*head);
+		*head = (*head)->next;
+	}
+	*head = NULL;
+}
