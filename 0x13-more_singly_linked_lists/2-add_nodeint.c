0830bb47f0057f6a6b9387a170771e465c1bc5d0
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-23 19:12:14 +0000

    add node

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * add_nodeint - print linked list
+ * @head: inked list head
+ * @n: number
+ * Return: pointer to new node
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+		if (new == NULL)
+			return (NULL);
+	(*new).n = n;
+	(*new).next = *head;
+	*head = new;
+	return (new);
+}
