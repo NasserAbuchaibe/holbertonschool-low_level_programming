daeba100ff7b8bddba3f57cbdb7751f8c7c7474c
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-24 18:15:31 +0000

    fix free4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,14 +6,12 @@
  */
 void free_listint(listint_t *head)
 {
-	listint_t *aux;
 
 	if (head == NULL)
 		return;
-	while ((*head).next != NULL)
+	while (head != NULL)
 	{
-		aux = (*head).next;
 		free(head);
-		head = aux;
+		head = (*head).next;
 	}
 }
