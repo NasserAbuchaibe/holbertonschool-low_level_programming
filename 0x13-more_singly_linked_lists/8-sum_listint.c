140176b21fc141eb52c1e543cdfd6108ae56abe3
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-24 01:40:21 +0000

    sum list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_listint - sum list
+ * @head: inked list head
+ * Return: sum list or 0
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		sum += (*head).n;
+		head = (*head).next;
+	}
+	return (sum);
+}
