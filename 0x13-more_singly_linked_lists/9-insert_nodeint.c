9de8d00686825b47839644dd0179bc040f92bf48
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-24 18:45:37 +0000

    fix2

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,8 +11,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int cont = 0;
 	listint_t *new, *aux;
 
+	if (head == NULL || *head == NULL)
+		return (NULL);
 	aux = *head;
-
 	idx -= 1;
 	while (aux != NULL)
 	{
