64e17c7872f6c99c8ef7112a75427668a7f9133a
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-24 18:07:43 +0000

    fix 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,6 +11,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	aux = *head;
 	new = malloc(sizeof(listint_t));
+		if (new == NULL)
+			return (NULL);
 	(*new).n = n;
 	(*new).next = NULL;
 	if (head == NULL || *head == NULL)
