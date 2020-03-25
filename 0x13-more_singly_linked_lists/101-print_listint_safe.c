5c16a474877d7f50b2741c78914f0e7789b9e9c6
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-25 19:33:30 +0000

    fix

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * print_listint_safe - print linked list
+ * @head: inked list head
+ * Return: number of node
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	unsigned int cont = 0;
+
+	if (head == NULL)
+	{
+		exit(98);
+	}
+	while (head != NULL)
+	{
+		cont++;
+		if (head - (head->next) > 0)
+		{
+			printf("[%p] %d\n", (void *)head, (*head).n);
+		}
+		else
+		{
+			printf("[%p] %d\n", (void *)head, (*head).n);
+			printf("-> [%p] %d\n", (void *)head, head->next->n);
+			break;
+		}
+		head = (*head).next;
+	}
+	return (cont);
+}
