a49324bb1257637142ce39c841e29aa6b0ed2b73
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-31 23:57:10 +0000

    copy

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,58 @@
+#include "holberton.h"
+void _close(int file);
+/**
+ * append_text_to_file- append text to file
+ * @filename: pointer to file
+ * @text_content: string to add
+ * Return: 1 or -1
+ */
+int main(int argc, char *av[])
+{
+	int filef, filet, filer, filew;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	filef = open(av[1], O_RDONLY);
+	if (filef == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	filet = open(av[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
+	if (filet == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	filer = read(filef, buffer, 1024);
+	if (filer == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	filew = write(filet, buffer, filer);
+	if (filew == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	_close(filef);
+	_close(filet);
+	return (0);
+}
+
+void _close(int file)
+{
+	int fail;
+
+	fail = close(file);
+	if (fail == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file);
+		exit(100);	
+	}
+}
