ab3de526f5c50ed2d7f135815bca4d4b2b2c1160
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-31 21:01:49 +0000

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
-		return (1);
+		text_content = "";
 	for (cont = 0; text_content[cont] != '\0'; cont++)
 	{
 	}
