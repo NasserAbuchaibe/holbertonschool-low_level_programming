627e5f3fe4d680306176cf3f75ab6a3de1124217
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-03-31 20:32:07 +0000

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * read_textfile - that reads a text file and prints
+ * create_file - that reads a text file and prints
  * it to the POSIX standard output.
  * @filename: pointer to file
  * @text_content: string
