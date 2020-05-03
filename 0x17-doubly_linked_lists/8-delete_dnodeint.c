76782bc5863b5f03f915ad4c76a5368be837819d
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-05-03 23:51:21 +0000

    8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -17,8 +17,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = (*head)->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
+		if ((aux)->next != NULL)
+			aux->next->prev = NULL;
 		free(aux);
 		return (1);
 	}
