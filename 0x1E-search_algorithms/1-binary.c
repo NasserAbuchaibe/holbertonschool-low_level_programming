0022d670b5801a32f27bf4b1106d6f625566daa0
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-08-25 21:32:36 +0000

    fix betty

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
-  * lbinary_search - function that searches for a value in an array of
+  * binary_search - function that searches for a value in an array of
   * integers using the binary search algorithm
   * @array: is a pointer to the first element of the array to search in.
   * @size: number of elements in array
