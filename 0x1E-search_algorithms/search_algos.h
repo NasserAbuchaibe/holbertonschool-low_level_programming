da3eb980c7586ebddcbbd2b84687df154f91ae1b
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-08-27 20:47:36 +0000

    fix

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,5 +8,6 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump(int *array, int start, int end, int size, int value);
 int jump_search(int *array, size_t size, int value);
+void func_print_array(int const *left, int const *right);
 int interpolation_search(int *array, size_t size, int value);
 #endif /* SEARCH_ALGOS_H */
